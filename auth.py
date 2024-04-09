29a82d13005858fe17064de78d6355c20d6919ee
[Muhammed Hisham] <[muhammedhisham2003@gmail.com]>
2024-04-09 18:12:44 +0530

    deleteUser added and wrote tests

--- auth.py
+++ auth.py
@@ -6,22 +6,35 @@ with open('./users.json', 'r') as users_file:
     data = users_file.read()
     users = json.loads(data)
 
+def update_users_json(users):
+    with open('./users.json', 'w') as users_file:
+        json.dump(users, users_file)
+
 def authenticate(username, password):
     if username not in users:
-        return 'UNF'
+        return 'UserNotFound'
     elif username in users and users[username] == password:
-        return 'ASF'
+        return 'AuthSuccess'
     else:
-        return 'ANS'
+        return 'AuthFail'
     
 def addUser(username, password):
     if username in users:
         return 'UsernameAlreadyExists'
-    else:
-        users[username] = password
-        with open('./users.json', 'w') as users_file:
-            json.dump(users, users_file)
-        return 'UserAddedSuccessfully'
+    
+    users[username] = password
+    update_users_json(users)
+    return 'UserAdded'
+    
+def deleteUser(username):
+    if username not in users:
+        return 'UserNotFound'
+    
+    del users[username]
+    update_users_json(users)
+    return 'UserDeleted'
+
+        
 
 
 
--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-from auth import authenticate, addUser
+from auth import authenticate, addUser, deleteUser
+
+print(
+    addUser('hisham', 'admin'),"\n", # add hisham
+    addUser('rohit', 'admin1'),"\n", # add rohit 
+
+    authenticate('hisham', 'admin'),"\n", # should be successful
+    authenticate('rohit', 'admin'),"\n", # should fail
+
+    deleteUser('hisham'),"\n" # delete hisham
+) 
